ce89d358513d048053faec2d8655419f569d2481
Hillol Talukdar <hillol@inverseai.com>
2024-12-31 09:37:53 +0600

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Hillol Talukdar on 31/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
