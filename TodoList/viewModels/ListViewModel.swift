2ab906efb45805aed7e3859087215b80bfe0acc1
Hillol Talukdar <hillol@inverseai.com>
2025-01-13 09:53:16 +0600

    Implement Save and persist data using UserDefaults

--- TodoList/viewModels/ListViewModel.swift
+++ TodoList/viewModels/ListViewModel.swift
@@ -8,20 +8,32 @@
 import Foundation
 
 class ListViewModel: ObservableObject {
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first item", isCompleted: false),
-            ItemModel(title: "This is the second item", isCompleted: true),
-            ItemModel(title: "This is the third item", isCompleted: false),
-        ]
+        //        let newItems = [
+        //            ItemModel(title: "This is the first item", isCompleted: false),
+        //            ItemModel(title: "This is the second item", isCompleted: true),
+        //            ItemModel(title: "This is the third item", isCompleted: false),
+        //        ]
+        //        items.append(contentsOf: newItems)
         
-        items.append(contentsOf: newItems)
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        self.items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -40,6 +52,12 @@ class ListViewModel: ObservableObject {
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex (where: { $0.id == item.id}) {
             items[index] = item.updateCompletion()
+        }
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
